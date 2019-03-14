c5ce950943c6d8c24d235789fff8ad13871b48d4
kdthanvi <thanvikaran95@gmail.com>
2019-03-14 12:06:09 +0900

    adding 01_palindrome

---
+++ 01/palindrome.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+// the function required!
+bool palindrome(string str){
+    int left = 0;
+    int right = str.length()-1;
+
+    for(int i = 0; i < str.length(); i++){
+        str[i] = tolower(str[i]);
+    }
+        
+    while (left <= right){
+
+        if (str[left] < 'a' || str[left] > 'z')
+            left++;
+        else if(str[right] < 'a' || str[right] > 'z')
+            right--;
+        else if (str[left] != str[right])
+            return false;
+        else
+            left++, right--;
+    }
+    return true;
+}
+
+// supporting main
+int main(){
+    string str;
+    cout << "Enter the string : ";
+    getline(cin, str);
+
+    if(palindrome(str))
+        cout << str << " is a palindrome!\n";
+    else
+        cout << str << " is NOT a palindrome!\n";
+    
+    return 0;
+}
